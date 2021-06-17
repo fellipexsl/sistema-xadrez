087c4abca74ac74436e25965a97c77cacdd47d8b
fellipe almeida <fellipexsl@hotmail.com>
2021-06-17 20:26:40 -0300

    ChessException e ChessPosition

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,22 @@
+package boardgame;
+
+
+public class BoardException extends RunTimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardException(String msg) {
+		super(msg);
+	}
+	public boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >=0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		return piece(position) != null;
+	}
+}
